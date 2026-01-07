ad5fc4eeaecd0d7e65265db1aab05664a4549f7e
radityapanca02 <radityapanca02@gmail.com>
2026-01-07 09:14:14 +0700

    Minor Edit Json

--- api/verify.js
+++ api/verify.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   await new Promise(resolve => setTimeout(resolve, 1200))
 
-  return res.status(403).json({
+  return res.status(200).json({
     status: false,
     message: "CRACK BY TG@resdinasin"
   })
