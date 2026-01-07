6d1fe3224cccee6b8e9f56e9b777c3eb35d9fe49
radityapanca02 <radityapanca02@gmail.com>
2026-01-07 09:16:30 +0700

    Minor Edit APIS

--- api/verify.js
+++ api/verify.js
@@ -2,8 +2,8 @@ export default async function handler(req, res) {
 
   if (req.method !== "POST") {
     return res.status(405).json({
-      status: "TG: @resdinasin",
-      message: "Please Don't Bully Me"
+      status: false,
+      message: "CRACK BY TG@resdinasin"
     })
   }
 
