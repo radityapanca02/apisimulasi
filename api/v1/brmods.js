b96ee82196eefba721fc3938b5063b1382459fd6
radityapanca02 <radityapanca02@gmail.com>
2026-01-08 20:45:38 +0700

    FIX APIS

--- api/v1/brmods.js
+++ api/v1/brmods.js
@@ -1,8 +1,28 @@
 export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(200).json({
-            Cliente: "RESDINAJAGO",
-            Dias: "2999"
-        });
-    }
+    return res.status(200).json({
+        status: true,
+        data: {
+            real: "BS-PBS-FREEMODSBSPREMIUM-FAKE-KEY",
+            token: "1234567890abcdef",
+            modname: "UnoShibai Hacks",
+            mod_status: "Safe",
+            credit: "OK",
+            ESP: true,
+            Item: true,
+            AIM: true,
+            SilentAim: true,
+            BulletTrack: true,
+            Floating: true,
+            Memory: true,
+            Setting: true,
+            EXP: "2099-12-31 23:59:59",
+            device: "999",
+            MOD_NAME: "UnoShibai Hacks",
+            MOD_STATUS: "Safe",
+            FLOTING_TEST: "OK",
+            BHATIA_EXP: "2099-12-31 23:59:59",
+            BHATIA_SLOT: "999",
+            rng: 999999999
+        }
+    });
 }
